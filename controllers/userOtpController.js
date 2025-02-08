c24f07f9089fb9a274a16aaeecb9a2b2e7d1f83a
lokesh143b <srirangamlokeswararao@gmail.com>
2025-02-08 17:53:51 +0530

    first commit

--- controllers/userOtpController.js
+++ controllers/userOtpController.js
@@ -1,7 +1,7 @@
 const User = require("../models/userModel");
 const nodemailer = require("nodemailer");
 const crypto = require("crypto");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const dotenv = require("dotenv");
 
 dotenv.config();
